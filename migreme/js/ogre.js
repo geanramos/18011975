f11a98a5c77a7474b2f47d46bf9e3a547d4fd8c1
Gean Ramos <geanramos@hotmail.com.br>
2025-04-19 02:23:49 -0300

    Update ogre.js

--- migreme/js/ogre.js
+++ migreme/js/ogre.js
@@ -1,10 +1,1 @@
-jQuery(document).ready(function($) {
-	$("#query").tweet({
-		avatar_size: 32,
-		count: 4,
-		query: "gean.me",
-		loading_text: "ogros trabalhando..."
-	});
-})
-
-}
+// Chamar a função ao carregar a página
